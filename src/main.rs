21ae9b09f099803f2b5fb7792672c40bb21e72b5
Andy Bright <abright@users.noreply.github.com>
2020-09-08 14:28:17 +1000

    Improve worker thread error handling

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use std::{
     io::Error as IoError,
-    net::TcpListener,
+    net::{TcpListener, TcpStream},
     sync::atomic::{AtomicUsize, Ordering},
     thread::Builder,
 };
 use thiserror::Error;
-use tungstenite::server::accept;
+use tungstenite::{
+    error::Error as WebSocketError, handshake::server::NoCallback, server::accept, HandshakeError,
+    ServerHandshake,
+};
 
 #[derive(Debug, Error)]
 enum ServerError {
@@ -17,9 +20,41 @@ enum ServerError {
     IncomingStream(#[source] IoError),
 }
 
+#[derive(Debug, Error)]
+// TODO: add some identifier to these errors (a thread id or port number maybe?)
+enum WorkerError {
+    #[error("unable to accept connection as WebSocket")]
+    WebSocketAccept(#[source] HandshakeError<ServerHandshake<TcpStream, NoCallback>>),
+    #[error("couldn't read WebSocket message")]
+    ReadMessage(#[source] WebSocketError),
+    #[error("couldn't write WebSocket message")]
+    WriteMessage(#[source] WebSocketError),
+}
+
 fn next_worker_id() -> usize {
     static WORKER_ID: AtomicUsize = AtomicUsize::new(0);
     WORKER_ID.fetch_add(1, Ordering::SeqCst)
+}
+
+fn handle_client(id: usize, stream: TcpStream) -> Result<(), WorkerError> {
+    let mut websocket = accept(stream).map_err(WorkerError::WebSocketAccept)?;
+    loop {
+        let msg = match websocket.read_message() {
+            Err(WebSocketError::ConnectionClosed) => {
+                println!("client disconnected from worker {}", id);
+                break;
+            }
+            a => a.map_err(WorkerError::ReadMessage),
+        }?;
+
+        // don't send back ping/pong messages
+        if msg.is_binary() || msg.is_text() {
+            websocket
+                .write_message(msg)
+                .map_err(WorkerError::WriteMessage)?;
+        }
+    }
+    Ok(())
 }
 
 fn run_server() -> Result<(), ServerError> {
@@ -28,17 +63,16 @@ fn run_server() -> Result<(), ServerError> {
     for stream in server.incoming() {
         let stream = stream.map_err(ServerError::IncomingStream)?;
         let worker_id = next_worker_id();
+        println!(
+            "spawning worker with id {} for {:?}",
+            worker_id,
+            stream.local_addr()
+        );
         Builder::new()
             .name(format!("worker-{}", worker_id))
             .spawn(move || {
-                let mut websocket = accept(stream).expect("can't accept connection");
-                loop {
-                    let msg = websocket.read_message().expect("can't read message");
-
-                    // don't send back ping/pong messages
-                    if msg.is_binary() || msg.is_text() {
-                        websocket.write_message(msg).expect("can't write message");
-                    }
+                if let Err(err) = handle_client(worker_id, stream) {
+                    eprintln!("error from worker thread {}: {}", worker_id, err)
                 }
             })
             .map_err(ServerError::SpawnThread)?;
