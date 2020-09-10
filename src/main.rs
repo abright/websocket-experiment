7b544fb99fb769a2c3a72326a567bc292b23205a
Andy Bright <abright@users.noreply.github.com>
2020-09-10 20:56:27 +1000

    Add TLS support with native-tls

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+use native_tls::{Error as TlsError, Identity, TlsAcceptor, TlsStream};
 use std::{
-    io::Error as IoError,
+    fs::File,
+    io::{Error as IoError, Read},
     net::{TcpListener, TcpStream},
-    sync::atomic::{AtomicUsize, Ordering},
+    sync::{
+        atomic::{AtomicUsize, Ordering},
+        Arc,
+    },
     thread::Builder,
 };
 use thiserror::Error;
@@ -12,8 +17,16 @@ use tungstenite::{
 
 #[derive(Debug, Error)]
 enum ServerError {
+    #[error("could not open certificate file")]
+    OpenCertificate(#[source] IoError),
+    #[error("could not read certificate file")]
+    ReadCertificate(#[source] IoError),
+    #[error("unable to create TLS identity from certificate")]
+    CreateTLSIdentity(#[source] TlsError),
     #[error("could not bind socket")]
     Bind(#[source] IoError),
+    #[error("could not create TLS acceptor")]
+    CreateTLSAcceptor(#[source] TlsError),
     #[error("unable to spawn worker thread")]
     SpawnThread(#[source] IoError),
     #[error("error accepting connection")]
@@ -24,7 +37,7 @@ enum ServerError {
 // TODO: add some identifier to these errors (a thread id or port number maybe?)
 enum WorkerError {
     #[error("unable to accept connection as WebSocket")]
-    WebSocketAccept(#[source] HandshakeError<ServerHandshake<TcpStream, NoCallback>>),
+    WebSocketAccept(#[source] HandshakeError<ServerHandshake<TlsStream<TcpStream>, NoCallback>>),
     #[error("couldn't read WebSocket message")]
     ReadMessage(#[source] WebSocketError),
     #[error("couldn't write WebSocket message")]
@@ -36,7 +49,7 @@ fn next_worker_id() -> usize {
     WORKER_ID.fetch_add(1, Ordering::SeqCst)
 }
 
-fn handle_client(id: usize, stream: TcpStream) -> Result<(), WorkerError> {
+fn handle_client(id: usize, stream: TlsStream<TcpStream>) -> Result<(), WorkerError> {
     let mut websocket = accept(stream).map_err(WorkerError::WebSocketAccept)?;
     loop {
         let msg = match websocket.read_message() {
@@ -58,8 +71,19 @@ fn handle_client(id: usize, stream: TcpStream) -> Result<(), WorkerError> {
 }
 
 fn run_server() -> Result<(), ServerError> {
-    // connect to ws://127.0.0.1:9002
+    let mut file = File::open("test.pfx").map_err(ServerError::OpenCertificate)?;
+    let mut identity = vec![];
+    file.read_to_end(&mut identity)
+        .map_err(ServerError::ReadCertificate)?;
+    let identity =
+        Identity::from_pkcs12(&identity, "test").map_err(ServerError::CreateTLSIdentity)?;
+
+    // connect to wss://localhost:9002
     let server = TcpListener::bind("127.0.0.1:9002").map_err(ServerError::Bind)?;
+
+    let acceptor = TlsAcceptor::new(identity).map_err(ServerError::CreateTLSAcceptor)?;
+    let acceptor = Arc::new(acceptor);
+
     for stream in server.incoming() {
         let stream = stream.map_err(ServerError::IncomingStream)?;
         let worker_id = next_worker_id();
@@ -68,11 +92,13 @@ fn run_server() -> Result<(), ServerError> {
             worker_id,
             stream.local_addr()
         );
+        let acceptor = acceptor.clone();
         Builder::new()
             .name(format!("worker-{}", worker_id))
             .spawn(move || {
+                let stream = acceptor.accept(stream).unwrap();
                 if let Err(err) = handle_client(worker_id, stream) {
-                    eprintln!("error from worker thread {}: {}", worker_id, err)
+                    eprintln!("error from worker thread {}: {:?}", worker_id, err)
                 }
             })
             .map_err(ServerError::SpawnThread)?;
